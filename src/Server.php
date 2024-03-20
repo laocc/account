7a4b5e388ad66227fe99545436d7d9e1e034c195
老船长 <fazo@qq.com>
2024-03-20 14:47:41 +0800

    Default Changelist

--- src/Server.php
+++ src/Server.php
@@ -2,6 +2,8 @@
 
 namespace laocc\account;
 
+use esp\weiXin\items\Auth;
+
 class Server extends Base
 {
     public string $token;
@@ -11,6 +13,12 @@ class Server extends Base
         $this->token = $token;
     }
 
+    /**
+     * 受理应用端发来的数据，验签
+     *
+     * @param bool $checkSign
+     * @return array|string
+     */
     public function post(bool $checkSign = true): array|string
     {
         $post = file_get_contents('php://input');
@@ -19,6 +27,23 @@ class Server extends Base
             if (getenv('HTTP_SIGN') !== $sign) return 'sign error';
         }
         return json_decode($post, true);
+    }
+
+    /**
+     * 组合微信登录URL
+     *
+     * @return string|void
+     */
+    public function auth(array $open)
+    {
+//        $open = $this->config('setup.wxweb');
+        if (empty($open)) return null;
+        extract($open + ['appid' => '', 'secret' => '', 'domain' => '', 'back' => '', 'redirect' => '']);
+        $auth = new Auth($appid, $secret);
+        if (!$domain) $domain = (_HTTPS ? 'https:' : 'http:') . '//' . _DOMAIN;//当前域名
+        if (!$redirect) $redirect = urlencode($domain . '/login/auth/');//微信跳回验证之后，再次跳转，这只是下面back的一部分
+        if (!$back) $back = "/login/redirect/login/{$redirect}";//微信跳回的URL
+        return $auth->redirect("{$domain}{$back}", 3);//微信跳回
     }
 
     /**
